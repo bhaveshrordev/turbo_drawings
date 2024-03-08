7e298e553b3206357bdfb25c2ba627bb88d203db
bhaveshrordev <salujabhavesh24@gmail.com>
2024-03-08 15:30:46 +0530

    added open and close dropdown stimulus action

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,7 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
+  
+  static targets = ["dropdownContent"]
+
   connect() {
+    this.dropdownContentTarget.hidden = true
     console.log('hello from dropdown stimulus')
+  }
+
+  openDropdown() {
+    this.dropdownContentTarget.hidden = false
+  }
+
+  closeDropdown() {
+    this.dropdownContentTarget.hidden = true
   }
 }
