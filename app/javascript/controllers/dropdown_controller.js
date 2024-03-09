86efe23c343c79204794ca547096a6ab5d3550e6
bhaveshrordev <salujabhavesh24@gmail.com>
2024-03-09 15:05:10 +0530

    added stimulus targets for open and close btn

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -3,18 +3,22 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="dropdown"
 export default class extends Controller {
   
-  static targets = ["dropdownContent"]
+  static targets = ["dropdownContent", "openButton", "closeButton"]
 
   connect() {
     this.dropdownContentTarget.hidden = true
-    console.log('hello from dropdown stimulus')
+    this.closeButtonTarget.hidden = true
   }
 
   openDropdown() {
     this.dropdownContentTarget.hidden = false
+    this.closeButtonTarget.hidden = false
+    this.openButtonTarget.hidden = true
   }
 
   closeDropdown() {
     this.dropdownContentTarget.hidden = true
+    this.openButtonTarget.hidden = false
+    this.closeButtonTarget.hidden = true
   }
 }
