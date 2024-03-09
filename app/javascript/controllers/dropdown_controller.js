1e6d462b6fa70ccd27346903f2ff8a731a82f1ac
bhaveshrordev <salujabhavesh24@gmail.com>
2024-03-09 15:19:43 +0530

    added toggleDropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -6,19 +6,31 @@ export default class extends Controller {
   static targets = ["dropdownContent", "openButton", "closeButton"]
 
   connect() {
-    this.dropdownContentTarget.hidden = true
-    this.closeButtonTarget.hidden = true
+    this.closeDropdown()
+  }
+
+  toggleDropdown() {
+    if (this.dropdownContentTarget.hidden == true) {
+      this.openDropdown()
+    }
+    else {
+      this.closeDropdown()
+    }
   }
 
   openDropdown() {
     this.dropdownContentTarget.hidden = false
-    this.closeButtonTarget.hidden = false
-    this.openButtonTarget.hidden = true
+    try {
+      this.closeButtonTarget.hidden = false
+      this.openButtonTarget.hidden = true
+    } catch {}
   }
 
   closeDropdown() {
     this.dropdownContentTarget.hidden = true
-    this.openButtonTarget.hidden = false
-    this.closeButtonTarget.hidden = true
+    try {
+      this.openButtonTarget.hidden = false
+      this.closeButtonTarget.hidden = true
+    } catch {}
   }
 }
